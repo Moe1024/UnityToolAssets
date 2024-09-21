c2e8f49688cde7dd502109c3e421b21fce0cfe57
Caviar <102456845+Moe1024@users.noreply.github.com>
2024-09-21 11:57:01 +0800

    Update

---
+++ HybridHotUpdate/HotUpdate/TestHotUpdate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TestHotUpdateNamespace
+{
+
+    public class TestHotUpdate : MonoBehaviour
+    {
+
+        public void Hello()
+        {
+            Debug.Log("Hello world!");
+
+        }
+
+
+    }
+}
+
